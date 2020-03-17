6f3597ba6a201bf076ac270b130502843bab70a9
Jason Abbott <dev@trailimage.com>
2020-03-17 13:09:09 -0600

    update deps

--- src/overpass.ts
+++ src/overpass.ts
@@ -1,28 +1,28 @@
-import { ItemType } from '.';
+import { ItemType } from '.'
 
 /**
  * Overapass API response
  */
 export interface OverpassResponse {
-   version: number;
-   generator: string;
-   osm3s: any;
-   elements: OverpassElement[];
+   version: number
+   generator: string
+   osm3s: any
+   elements: OverpassElement[]
 }
 
 export interface OverpassElement {
-   type: ItemType;
-   id: number;
-   tags?: { [key: string]: string };
+   type: ItemType
+   id: number
+   tags?: { [key: string]: string }
 }
 
 export interface OverpassNode extends OverpassElement {
-   type: ItemType.Node;
-   lat: number;
-   lon: number;
+   type: ItemType.Node
+   lat: number
+   lon: number
 }
 
 export interface OverpassWay extends OverpassElement {
-   type: ItemType.Way;
-   nodes: number[];
+   type: ItemType.Way
+   nodes: number[]
 }
